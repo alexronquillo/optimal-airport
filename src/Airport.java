07606cd449b85a2a39dc4c6c1aa18612a8d282a8
rpont <ryanppont@gmail.com>
2015-03-31 18:12:25 -0400

    Added summary comments to Airport.java

--- src/Airport.java
+++ src/Airport.java
@@ -1,3 +1,15 @@
+/* A simple airport system consists of arriving airplanes, departing airplanes, 
+ * and airplanes at gates and loading unloading areas (referred to in this 
+ * documentation as bays). Airplanes approach the airport and communicate with 
+ * the air traffic controller to determine when the plane can land. Once the 
+ * landing conditions have been met, the airplane begins its landing procedure. 
+ * After landing, the plane awaits an available gate or bay depending on the plane 
+ * type. After a gate or bay becomes available, the plane will approach the gate 
+ * and await passenger/cargo loading/unloading. After the plane has received its 
+ * services at the gate/bay, the plane coordinates with the air traffic controller 
+ * to approach the departure runway. When sufficient conditions have been met, 
+ * the plane will enter the runway and takeoff.
+ */
 
 public class Airport {
 
