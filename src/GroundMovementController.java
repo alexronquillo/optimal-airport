ec6a4c99fa362010b30be0177d93846b2ba68313
rpont <ryanppont@gmail.com>
2015-04-02 19:53:10 -0400

    Added NullServiceAreaException, finished implementation of GroundMovementController

--- src/GroundMovementController.java
+++ src/GroundMovementController.java
@@ -1,55 +1,77 @@
 
-
 public class GroundMovementController implements Runnable {
-	
+
 	@Override
 	public void run() {
-		while(true) {
-			if(nextIsPassengerPlane()) {
-				while(!gateAvailable());
-				Gate gate = getGate();
-				PassengerPlane plane = (PassengerPlane)getNextPlane();
-				setPassengerPlaneToGate(gate, plane);
-			} else {
-				while (!cargoBayAvailable());
-				CargoBay bay = getCargoBay();
-			    CargoPlane plane = (CargoPlane)getNextPlane();
-				setCargoPlaneToCargoBay(bay, plane);
+		try {
+			while (true) {
+				if (nextIsPassengerPlane()) {
+					while (!gateAvailable());
+					Gate gate;
+					gate = getGate();
+					PassengerPlane plane = (PassengerPlane) getNextPlane();
+					setPassengerPlaneToGate(gate, plane);
+				} else {
+					while (!cargoBayAvailable());
+					CargoBay bay = getCargoBay();
+					CargoPlane plane = (CargoPlane) getNextPlane();
+					setCargoPlaneToCargoBay(bay, plane);
+				}
 			}
+		} catch (NullServiceAreaException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	private static boolean gateAvailable() {
-		return false;	
+		for (Gate gate : Airport.getGates()) {
+			if (gate.isAvailable()) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
+
 	private static boolean cargoBayAvailable() {
-		return false;	
+		for (CargoBay bay : Airport.getBays()) {
+			if (bay.isAvailable()) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
-	private static Gate getGate() {
-		return null;
+
+	private static Gate getGate() throws NullServiceAreaException {
+		for (Gate gate : Airport.getGates()) {
+			if (gate.isAvailable()) {
+				return gate;
+			}
+		}
+		throw new NullServiceAreaException("Ground Movement receives null gate");
 	}
-	
-	private static CargoBay getCargoBay() {
-		return null;
+
+	private static CargoBay getCargoBay() throws NullServiceAreaException {
+		for (CargoBay bay : Airport.getBays()) {
+			if (bay.isAvailable()) {
+				return bay;
+			}
+		}
+		throw new NullServiceAreaException("Ground Movement receives null bay");
 	}
-	
+
 	private static void setPassengerPlaneToGate(Gate gate, PassengerPlane plane) {
 		gate.service(plane);
 	}
-	
+
 	private static void setCargoPlaneToCargoBay(CargoBay bay, CargoPlane plane) {
 		bay.service(plane);
 	}
-	
+
 	private static boolean nextIsPassengerPlane() {
-		return Airport.getLandedQueue().peek() instanceof PassengerPlane;	
+		return Airport.getLandedQueue().peek() instanceof PassengerPlane;
 	}
 
-	
-	
 	private static Airplane getNextPlane() {
-		return null;
+		return Airport.getLandedQueue().poll();
 	}
 }
---
+++ src/NullServiceAreaException.java
@@ -0,0 +1,8 @@
+public class NullServiceAreaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NullServiceAreaException(String message) {
+		super(message);
+	}
+}
