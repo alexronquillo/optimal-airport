25475b680a7fff594dca7c6e15d4a5c1d58fce0c
rpont <ryanppont@gmail.com>
2015-04-02 19:30:33 -0400

    Added Gates and Bays

--- src/Airport.java
+++ src/Airport.java
@@ -22,17 +22,27 @@ public class Airport {
 	private static final int ARRIVALS_QUEUE_CAPACITY = 100;
 	private static final int LANDED_QUEUE_CAPACITY = 2;
 	private static final int DEPARTURE_QUEUE_CAPACITY = 100;
+	private static final int NUM_GATES = 5;
+	private static final int NUM_BAYS = 5;
 	private static BlockingQueue<Runway> runways = initializeRunways();
+	private static Gate[] gateArray = initializeGates();
+	private static CargoBay[] bayArray = initializeCargoBays();
 	private static BlockingQueue<Airplane> arrivalsQueue = new PriorityBlockingQueue<>(ARRIVALS_QUEUE_CAPACITY);
 	private static BlockingQueue<Airplane> landedQueue = new ArrayBlockingQueue<>(LANDED_QUEUE_CAPACITY);
 	private static BlockingQueue<Airplane> departureQueue = new ArrayBlockingQueue<>(DEPARTURE_QUEUE_CAPACITY);
 	
-	public static void main(String[] args) {
-		AirTrafficController.start();
-	}
+	
 	
 	public static BlockingQueue<Runway> getRunways() {
 		return runways;
+	}
+	
+	public static Gate[] getGateAvailable() {
+		return gateArray;
+	}
+	
+	public static CargoBay[] getBayAvailable() {
+		return bayArray;
 	}
 	
 	public static BlockingQueue<Airplane> getArrivalsQueue() {
@@ -54,5 +64,21 @@ public class Airport {
 		}
 		
 		return runways;
+	}
+	
+	private static Gate[] initializeGates() {
+		Gate[] gateArray = new Gate[NUM_GATES];
+		for (int i = 0; i < NUM_GATES; ++i) {
+			gateArray[i] = new Gate();
+		}
+		return gateArray;
+	}
+	
+	private static CargoBay[] initializeCargoBays() {
+		CargoBay[] bayArray = new CargoBay[NUM_BAYS];
+		for (int i = 0; i < NUM_BAYS; ++i) {
+			bayArray[i] = new CargoBay();
+		}
+		return bayArray;
 	}
 }
