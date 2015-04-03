482387c7eea502270a13dbb634a66c49e7054bc5
alexronquillo <alex.ronquillo21@yahoo.com>
2015-04-03 11:36:43 -0400

    Implemented new abstract getServiceTime(Airplane) method with dummy values.

--- src/CargoPlaneServiceBehavior.java
+++ src/CargoPlaneServiceBehavior.java
@@ -1,21 +1,28 @@
 
 public class CargoPlaneServiceBehavior implements ServiceBehavior {	
-	int serviceTime = 3000;
 	@Override
 	public void service(Airplane airplane) throws InvalidAirplaneTypeException, InterruptedException {
-		try {
-			if (airplane instanceof CargoPlane) {
-				System.out.println("Start: Servicing a cargo plane");
-				Thread.sleep(serviceTime);
-				System.out.println("Finish: Servicing a cargo plane");
-			} else {
-				throw new InvalidAirplaneTypeException("Can only service a cargo plane");
-			}
+		if (airplane instanceof CargoPlane) {
+			System.out.println("Start: Servicing a cargo plane");
+			Thread.sleep(getServiceTime(airplane));
+			System.out.println("Finish: Servicing a cargo plane");
+		} else {
+			throw new InvalidAirplaneTypeException("Can only service a cargo plane");
 		}
-		catch (InterruptedException e) {
-			System.out.println(e.getClass().getName() + "----" + e.getMessage());
+	}
+	
+	@Override
+	public long getServiceTime(Airplane airplane) {
+		switch (airplane.getSize()) {
+			case SMALL:
+				return 2000L;
+			case MEDIUM:
+				return 3000L;				
+			case LARGE:
+				return 4000L;
+			default:
+				return 5000L;
 		}
-		
 	}
 
 }
--- src/PassengerPlaneServiceBehavior.java
+++ src/PassengerPlaneServiceBehavior.java
@@ -1,19 +1,27 @@
 
 public class PassengerPlaneServiceBehavior implements ServiceBehavior {
-	private int serviceTime = 1000;
 	@Override
-	public void service(Airplane airplane) throws InvalidAirplaneTypeException, InterruptedException {
-		try {
-			if (airplane instanceof PassengerPlane) {
-				System.out.println("Start: Servicing a passenger plane");
-				Thread.sleep(serviceTime);
-				System.out.println("Finish: Servicing a passenger plane");
-			} else {
-				throw new InvalidAirplaneTypeException("Can only service a passenger plane");
-			}
+	public void service(Airplane airplane) throws InterruptedException, InvalidAirplaneTypeException {	
+		if (airplane instanceof PassengerPlane) {
+			System.out.println("Start: Servicing a passenger plane");
+			Thread.sleep(getServiceTime(airplane));
+			System.out.println("Finish: Servicing a passenger plane");
+		} else {
+			throw new InvalidAirplaneTypeException("Can only service a passenger plane");
 		}
-		catch (Exception e) {
-			System.out.println(e.getClass().getName() + "----" + e.getMessage());
+	}
+	
+	@Override
+	public long getServiceTime(Airplane airplane) {		
+		switch (airplane.getSize()) {
+			case SMALL:
+				return 1000L;
+			case MEDIUM:
+				return 2000L;				
+			case LARGE:
+				return 3000L;
+			default:
+				return 4000L;
 		}
 	}
 }
