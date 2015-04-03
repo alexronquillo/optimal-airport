4de214b181d64dea54f412a3d13f18907d806f9d
alexronquillo <alex.ronquillo21@yahoo.com>
2015-04-03 10:47:46 -0400

    Removed the bulky block comment that describes the simulation. That information is in the documentation so is redundant.

--- src/Airport.java
+++ src/Airport.java
@@ -2,20 +2,6 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
-
-/* A simple airport system consists of arriving airplanes, departing airplanes, 
- * and airplanes at gates and loading unloading areas (referred to in this 
- * documentation as bays). Airplanes approach the airport and communicate with 
- * the air traffic controller to determine when the plane can land. Once the 
- * landing conditions have been met, the airplane begins its landing procedure. 
- * After landing, the plane awaits an available gate or bay depending on the plane 
- * type. After a gate or bay becomes available, the plane will approach the gate 
- * and await passenger/cargo loading/unloading. After the plane has received its 
- * services at the gate/bay, the plane coordinates with the air traffic controller 
- * to approach the departure runway. When sufficient conditions have been met, 
- * the plane will enter the runway and takeoff.
- */
-
 public class Airport {
 	
 	private static final int NUM_RUNWAYS = 2;
