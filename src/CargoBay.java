550ffa7bd8468a326f7f748915988bd0689d9da4
alexronquillo <alex.ronquillo21@yahoo.com>
2015-04-03 11:35:38 -0400

    Implemented the new abstract cleanupServiceArea() method with dummy values.

--- src/CargoBay.java
+++ src/CargoBay.java
@@ -3,4 +3,9 @@ public class CargoBay extends ServiceArea {
 	public CargoBay() {
 		super(new CargoPlaneServiceBehavior());
 	}
+
+	@Override
+	protected void cleanupServiceArea() throws InterruptedException {
+		Thread.sleep(2000);
+	}
 }
--- src/Gate.java
+++ src/Gate.java
@@ -3,4 +3,9 @@ public class Gate extends ServiceArea {
 	public Gate() {
 		super(new PassengerPlaneServiceBehavior());
 	}
+
+	@Override
+	protected void cleanupServiceArea() throws InterruptedException {
+		Thread.sleep(1000);
+	}
 }
