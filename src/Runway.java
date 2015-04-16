19b0c1034515d80a60291fa3dd57d56f1a4da516
alexronquillo <alex.ronquillo21@yahoo.com>
2015-04-16 07:52:23 -0400

    Added field and methods for Runway use calculations

--- src/Runway.java
+++ src/Runway.java
@@ -1,4 +1,12 @@
 
 public class Runway {
-
+	private long timeInUse = 0;
+	
+	public long getTimeInUse() {
+		return timeInUse;
+	}
+	
+	public void setTimeInUse(long timeInUse) {
+		this.timeInUse = timeInUse;
+	}
 }
