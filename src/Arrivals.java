bd925fb72422c2fff49b1466f92eca49569fe258
alexronquillo <alex.ronquillo21@yahoo.com>
2015-04-20 19:21:13 -0400

    Refactored code for code style

--- src/Arrivals.java
+++ src/Arrivals.java
@@ -1,9 +1,6 @@
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
-import javax.swing.JOptionPane;
-
-
 public class Arrivals implements Runnable {
 	private final int PERCENTAGE_OF_PLANES_AS_PASSENGER = 75;
 	private final int AVERAGE_NUMBER_OF_FLIGHTS_PER_DAY = 2400;
@@ -12,14 +9,7 @@ public class Arrivals implements Runnable {
 	private final double LANDING_FACTOR = 0.006944444;
 
 	private Random generator = new Random();
-	private int maxSizeOfArrivals = 0;
 	private int numberOfPlanes = 0;
-	private double meanInterArrivalTime;
-	
-	public Arrivals() {
-		meanInterArrivalTime = Airport.ARRIVAL_PERIOD / AVERAGE_NUMBER_OF_FLIGHTS_PER_DAY;
-		maxSizeOfArrivals = Airport.ARRIVALS_QUEUE_CAPACITY;
-	}
 	
 	@Override
 	public void run() {		
@@ -32,22 +22,21 @@ public class Arrivals implements Runnable {
 			while ((totalElapsedTime = (System.currentTimeMillis() - startTime) / 1000) <= Airport.ARRIVAL_PERIOD) {
 				double arrivalTime = getArrivalTime();
 
-				if ((totalElapsedTime-elapsedTimeSincePlaneGenerated) > arrivalTime) {
+				if (totalElapsedTime - elapsedTimeSincePlaneGenerated > arrivalTime) {
 					Airplane plane = generatePlane();
 					plane.startWait();
+					elapsedTimeSincePlaneGenerated = totalElapsedTime;
 
 					boolean success = false;
-					if (maxSizeOfArrivals >= arrivalsQueue.size()) {
+					if (Airport.ARRIVALS_QUEUE_CAPACITY >= arrivalsQueue.size()) {
 						success = arrivalsQueue.offer(plane);
 					}
-					elapsedTimeSincePlaneGenerated = totalElapsedTime;
+
 					if (!success) {
 						rejectPlane(plane);
 					}
 				}
 			}
-
-			System.out.println("Arrivals has stopped generating new planes.");
 		} else {
 			System.out.println("Error: Arrivals queue is null");
 		}
@@ -91,6 +80,7 @@ public class Arrivals implements Runnable {
 	public double getArrivalTime() {
 		int max = 25;
 		int min = 0;
+		double meanInterArrivalTime = Airport.ARRIVAL_PERIOD / AVERAGE_NUMBER_OF_FLIGHTS_PER_DAY;
 
 		// Get a value anywhere from 75% to 125% of the mean value
 		double salt = .88 + ((double)generator.nextInt((max-min)+1) + min) / 100;
